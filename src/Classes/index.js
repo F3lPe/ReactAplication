7eef6b08f2dfd346d8832a2081a0f42cfc8b9c0d
F3lPe <felipegonsalves168@gmail.com>
2024-07-31 13:25:39 -0300

    31/07

--- src/Classes/index.js
+++ src/Classes/index.js
@@ -3,7 +3,11 @@ import React from "react"
 class IndexClasses extends React.Component{
     constructor(props){
         super(props);
-        this.state = {nome: "Felipe", counter: 0}   
+        this.state = {
+            nome: "Felipe",
+            counter: 0,
+            listagem: []
+        }
         this.valores = {december: 1599.34 -  (245.95 + 232.67), nextYear: null}    
     }
 
@@ -18,14 +22,46 @@ class IndexClasses extends React.Component{
         console.log(1100 - (150 + 300 + 239.88))
         console.log(this.state)
     }
+
+    getListagem(){
+        fetch('http://localhost:3001/users', {
+            method: 'GET',
+        })
+            .then(response => {
+
+                if(response.ok) {
+                    return response.json()
+                } else{
+                    const data = response.text()
+
+                    throw new Error(data.then(e => {
+                        return e
+                    }))
+                }
+
+            }).then(data =>{
+                this.setState({listagem: data})
+            }).catch(erro => {
+                console.log(erro)
+            })
+    }
     render(){
-        const {nome, counter} = this.state
-        
+        const {nome, counter,listagem} = this.state
         return(
             <div style={{display:"flex", justifyContent: "space-evenly", width: "100%", height: "100%"}}>  
-                <p onClick={() => this.handleClick()}>
+                <p onClick={() => this.getListagem()}>
                         {`${nome} ${counter}`}
                 </p>
+
+                <div>
+                    {listagem?.map(e => (
+                        <p key={e.id}>
+                            {e.name}
+                        </p>
+                        ))
+                    }
+
+                </div>
                 
             </div>
         )
